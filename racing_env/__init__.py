84958b14b120f61f31ae78b85a0b8b5db1df9657
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2024-11-20 16:28:46 -0500

    added dynamics model and track observer module in racing env module init registered racing env in gymnasium

--- racing_env/__init__.py
+++ racing_env/__init__.py
@@ -1,1 +1,11 @@
+from gymnasium import register
+
+import dynamics_model
+import track_observer
+
 from .racing_env import RacingEnv as RacingEnv
+
+register(
+    id="pilot_gym/RacingEnv-v0",
+    entry_point="RacingEnv",
+)
