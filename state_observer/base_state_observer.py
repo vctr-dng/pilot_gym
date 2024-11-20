a4439d3da2f1affd0a25020ee775b5dca395502b
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2024-11-20 17:19:28 -0500

    state observer component

---
+++ state_observer/base_state_observer.py
@@ -0,0 +1,26 @@
+from components_registry import register
+from dynamics_model import DynamicModel
+
+
+@register("state_observer/base_state_observer")
+class BaseStateObserver:
+    def __init__(self, dynamic_model: DynamicModel, observed_state: list):
+        self.dynamic_model = dynamic_model
+        self.observed_state = observed_state
+
+    def observe(self) -> dict:
+        observation = dict()
+
+        for state in self.observed_state:
+            try:
+                observation[state] = self.dynamic_model[state]
+            except (KeyError, TypeError):
+                try:
+                    observation[state] = getattr(self.dynamic_model, state)
+                except AttributeError:
+                    raise Exception(
+                        f"State '{state}' not found in dynamic model\
+                        {self.dynamic_model.__class__}"
+                    )
+
+        return observation
