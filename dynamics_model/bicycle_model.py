2932ced92426ca2e5e1044adb171cbe9dd573761
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2024-11-27 16:20:25 -0500

    bicycle model improvement and initializer component

--- dynamics_model/bicycle_model.py
+++ dynamics_model/bicycle_model.py
@@ -1,14 +1,16 @@
 import numpy as np
 
+from components_registry import register
 from dynamics_model import DynamicModel
 
 
+@register("dynamic_model/bicycle")
 class BicycleModel(DynamicModel):
-    actions = {
+    actions = [
         "throttle",
         "braking",
         "steering_rate",
-    }
+    ]
 
     def __init__(self, vehicle_params: dict, simulation_params: dict):
         self.x = 0  # m
@@ -50,6 +52,9 @@ class BicycleModel(DynamicModel):
 
         self.acceleration = (
             throttle * self.max_acceleration - braking * self.min_acceleration
+        )
+        self.acceleration = np.clip(
+            self.acceleration, self.min_acceleration, self.max_acceleration
         )
 
         self.steering += steering_rate * self.dt
