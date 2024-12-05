82c500557f7d76c567c71b044bf74cf0a4bdf90f
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2024-12-05 02:13:52 -0500

    smooth input reward models

--- reward_model/__init__.py
+++ reward_model/__init__.py
@@ -1,2 +1,6 @@
 from .base_reward_model import BaseRewardModel as BaseRewardModel
+from .controlled_input_reward_model import (
+    ControlledInputRewardModel as ControlledInputRewardModel,
+)
 from .simple_reward_model import SimpleRewardModel as SimpleRewardModel
+from .smooth_input_reward_model import SmoothInputRewardModel as SmoothInputRewardModel
---
+++ reward_model/smooth_input_reward_model.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+from components_registry import register
+
+from .base_reward_model import BaseRewardModel
+
+
+@register("reward_model/smooth_input_reward")
+class SmoothInputRewardModel(BaseRewardModel):
+    def __init__(self, coefficients: dict):
+        super().__init__(coefficients)
+        self.previous_action: dict = {
+            "steering_rate": 0,
+            "throttle": 0,
+            "braking": 0,
+        }
+        self.dt = 0.1
+
+    def __call__(
+        self,
+        action: dict,
+        state_observation: dict,
+        track_observation: np.array,
+        non_observable_states: dict,
+    ) -> float:
+        reward = 0
+
+        progress = non_observable_states["progress"]
+        lateral_proportion = track_observation[0]
+
+        progress_term = self.coefficients["progress"] * progress
+
+        out_track_term = 0
+        if np.abs(lateral_proportion) > 1:
+            progress_term = 0
+            out_track_term = -self.coefficients["out_track"] * lateral_proportion
+
+        # promote smoother steering inputs
+        steer_cost = -self.coefficients["steering_cost"] * self.input_cost(
+            self.previous_action["steering_rate"],
+            action["steering_rate"],
+            self.coefficients["steering_cost_pow"],
+        )
+        throttle_cost = -self.coefficients["throttle_cost"] * self.input_cost(
+            self.previous_action["throttle"],
+            action["throttle"],
+            self.coefficients["throttle_cost_pow"],
+        )
+        brake_cost = -self.coefficients["braking_cost"] * self.input_cost(
+            self.previous_action["braking"],
+            action["braking"],
+            self.coefficients["braking_cost_pow"],
+        )
+
+        reward = (
+            progress_term + out_track_term + steer_cost + throttle_cost + brake_cost
+        )
+
+        self.previous_action = action
+
+        reward_info = {
+            "progress": progress_term,
+            "out_track": out_track_term,
+            "steering_cost": steer_cost,
+            "throttle_cost": throttle_cost,
+            "braking_cost": brake_cost,
+        }
+
+        return reward, reward_info
+
+    def input_cost(self, old_action, current_action, pow):
+        return np.power(np.abs(old_action - current_action), pow) / self.dt
