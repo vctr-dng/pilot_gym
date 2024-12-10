6fadcb879ece356ec1cd7220e4322e672f706110
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2024-12-10 21:38:22 +0100

    removed circular import bug

--- dynamics_model/__init__.py
+++ dynamics_model/__init__.py
@@ -1,3 +1,3 @@
-from .bicycle_model import BicycleModel as BicycleModel
 from .dynamic_model import DynamicModel as DynamicModel
+from .bicycle_model import BicycleModel as BicycleModel
 from .kinematic_bicycle_model import KinematicBicycleModel as KinematicBicycleModel
